da89ac2ec5358bad3f137a53cee7e88074b19ec8
ckx0709 <178709230@qq.com>
2021-12-01 09:16:55 +0800

    emqx_auth_mnesia 认证方式，明文认证链接

--- hub/mqtt/auth_mnesia/main.go
+++ hub/mqtt/auth_mnesia/main.go
@@ -18,21 +18,41 @@ var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
 	fmt.Printf("***********************************************MSG  : %s\n", msg.Payload())
 }
 
+var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("Connected")
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	fmt.Printf("Connect lost: %v", err)
+}
+
 func main() {
-/*	bytes := md5.Sum([]byte("ZGZYZZJ-001-CKX"))
-	fmt.Printf("sum =%s\n", bytes)*/
+	/*	bytes := sha256.Sum256([]byte("ZGZYZZJ-001-CKX"))
+		//str := fmt.Sprintf("%s", bytes[:])
+		toString := base64.StdEncoding.EncodeToString(bytes[:])
+		fmt.Printf("str = %s\nstr1 =%s\n", toString, string(bytes[:]))*/
 
 	mqtt.DEBUG = log.New(os.Stdout, "", 0)
 	mqtt.ERROR = log.New(os.Stdout, "", 0)
 	opts := mqtt.NewClientOptions().AddBroker("tcp://47.98.231.220:1883").
 		SetClientID("community-health-device:123456").
-		SetUsername("admin").
+		SetUsername("adminCKX001").
 		SetPassword("ZGZYZZJ-001-CKX")
+
+	//使用证书登录
+	//opts.SetTLSConfig(tlsConfig)
 
 	opts.SetKeepAlive(60 * time.Second)
 	// 设置消息回调处理函数
 	opts.SetDefaultPublishHandler(f)
 	opts.SetPingTimeout(1 * time.Second)
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
 
 	c := mqtt.NewClient(opts)
 	if token := c.Connect(); token.Wait() && token.Error() != nil {
@@ -41,4 +61,16 @@ func main() {
 
 	fmt.Printf("#####conn = %t", c.IsConnected())
 
+	pubServer(c)
+
+}
+
+func pubServer(c mqtt.Client) {
+	topic := "/healthbot/to_service/1b3e632bc4324a9c8ac1930988f294af/376e54a23d014312b28fa52a6d91e1bd/status"
+	for i := 0; i < 100; i++ {
+		// 发布消息
+		token := c.Publish(topic, 0, false, "online")
+		token.Wait()
+		<-time.After(3 * time.Second)
+	}
 }
---
+++ hub/mqtt/auth_mnesia/tls.go
@@ -0,0 +1,36 @@
+/*
+@Time   : 2021/11/30 18:26
+@Author : ckx0709
+@Remark :
+*/
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+	"log"
+)
+
+var tlsConfig = NewTlsConfig()
+
+func NewTlsConfig() *tls.Config {
+	certpool := x509.NewCertPool()
+	ca, err := ioutil.ReadFile("ca.pem")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	certpool.AppendCertsFromPEM(ca)
+	// Import client certificate/key pair
+	clientKeyPair, err := tls.LoadX509KeyPair("client-crt.pem", "client-key.pem")
+	if err != nil {
+		panic(err)
+	}
+	return &tls.Config{
+		RootCAs:            certpool,
+		ClientAuth:         tls.NoClientCert,
+		ClientCAs:          nil,
+		InsecureSkipVerify: true,
+		Certificates:       []tls.Certificate{clientKeyPair},
+	}
+}
