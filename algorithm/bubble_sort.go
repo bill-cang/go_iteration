163c1a170eea99212984a773d572e10f41aebbb2
ckx0709 <178709230@qq.com>
2021-12-24 17:13:14 +0800

    algorithm-冒泡排序

--- algorithm/bubble_sort.go
+++ algorithm/bubble_sort.go
@@ -1,6 +1,22 @@
 /*
-@Time   : 2021/12/24 16:52 
+@Time   : 2021/12/24 16:52
 @Author : ckx0709
-@Remark :
+@Remark : 冒泡排序
 */
 package algorithm
+
+func BubbleSort(numbers []int) (sortNub []int) {
+
+	temp := 0
+	for i := 0; i < len(numbers); i++ {
+		for k := i + 1; k < len(numbers); k++ {
+			if numbers[i] < numbers[k] {
+				temp = numbers[i]
+				numbers[i] = numbers[k]
+				numbers[k] = temp
+			}
+		}
+	}
+
+	return numbers
+}
--- algorithm/bubble_sort_test.go
+++ algorithm/bubble_sort_test.go
@@ -1,6 +1,17 @@
 /*
-@Time   : 2021/12/24 16:57 
+@Time   : 2021/12/24 16:57
 @Author : ckx0709
 @Remark :
 */
 package algorithm
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBubbleSort(t *testing.T) {
+	nubs := []int{5, 3, 2, 1, 9, 4, 7, 5, 8, 6, 3}
+	sort := BubbleSort(nubs)
+	fmt.Printf("%+v", sort)
+}
