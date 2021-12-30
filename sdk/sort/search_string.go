0e10b49a03fea823361700ddcf0411fa65fc3318
ckx0709 <178709230@qq.com>
2021-12-30 10:00:53 +0800

    sdk-sort-search*

--- sdk/sort/search_string.go
+++ sdk/sort/search_string.go
@@ -1,6 +1,18 @@
 /*
-@Time   : 2021/12/29 15:41 
+@Time   : 2021/12/29 15:41
 @Author : ckx0709
 @Remark :
 */
 package sort
+
+import "sort"
+
+func SearchStringFromSplice(strSpl []string, str string) int {
+	/*
+	SearchStrings在递增顺序的a中搜索x，返回x的索引。
+	如果查找不到，返回值是x应该插入a的位置（以保证a的递增顺序），返回值可以是len(a)。
+	*/
+	return sort.SearchStrings(strSpl, str)
+}
+
+
--- sdk/sort/search_string_test.go
+++ sdk/sort/search_string_test.go
@@ -1,6 +1,25 @@
 /*
-@Time   : 2021/12/29 15:44 
+@Time   : 2021/12/29 15:44
 @Author : ckx0709
 @Remark :
 */
 package sort
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSearchStringFromSplice(t *testing.T) {
+	ss := []string{"a", "v", "s", "f", "r"}
+	ind := SearchStringFromSplice(ss, "z")
+	fmt.Println(ind)
+}
+
+/*
+circular	圆形的，环形的		记忆技巧：circ 圆，环 + ular 有…形状或性质的 → 圆形的
+oval 	椭圆形的		the oval office 椭圆办公室->隐身指美国总统
+ark		弧形
+segment		弓形		orange segments 橘子瓣
+*/
+
