b1772622192c2ace9c28ff46c373a925dc73f3f8
ckx0709 <178709230@qq.com>
2021-12-31 10:38:18 +0800

    diff usage

--- sdk/sort/search_string_test.go
+++ sdk/sort/search_string_test.go
@@ -7,6 +7,7 @@ package sort
 
 import (
 	"fmt"
+	"net/url"
 	"testing"
 )
 
@@ -17,9 +18,14 @@ func TestSearchStringFromSplice(t *testing.T) {
 }
 
 /*
-circular	圆形的，环形的		记忆技巧：circ 圆，环 + ular 有…形状或性质的 → 圆形的
+circular [ˈsɜːrkjələr]	圆形的，环形的		记忆技巧：circ 圆，环 + ular 有…形状或性质的 → 圆形的
 oval 	椭圆形的		the oval office 椭圆办公室->隐身指美国总统
 ark		弧形
 segment		弓形		orange segments 橘子瓣
 */
+
+func TestSearchStringFromSplice2(t *testing.T) {
+	escape := url.QueryEscape("2021-12-24T17:18:01+08:00")
+	fmt.Println(escape)
+}
 
