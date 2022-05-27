6cb666cdcf05d25eee74bcbc08c0ea4448991665
ckx0709 <178709230@qq.com>
2022-05-27 10:43:07 +0800

    优化

---
+++ hub/gui1/main.go
@@ -0,0 +1,27 @@
+/*
+@Time   : 2022/5/24 10:36
+@Author : ckx0709
+@Remark :
+*/
+package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello")
+
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
+
+	w.ShowAndRun()
+}
