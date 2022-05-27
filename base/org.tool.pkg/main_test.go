efe5f3082a07a8b3bdfeb77c327072c71eadedc8
ckx0709 <178709230@qq.com>
2022-05-27 10:41:10 +0800

    go:embed 使用

---
+++ base/org.tool.pkg/main_test.go
@@ -0,0 +1,42 @@
+/*
+@Time   : 2022/5/26 15:58
+@Author : ckx0709
+@Remark :
+*/
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestParseStruct(t *testing.T) {
+	var num float64 = 1.2345
+	fmt.Println("old value of pointer:", num)
+
+	// 通过reflect.ValueOf获取num中的reflect.Value，注意，参数必须是指针才能修改其值
+	pointer := reflect.ValueOf(&num)
+	newValue := pointer.Elem()
+
+	fmt.Println("type of pointer:", newValue.Type())
+	fmt.Println("settability of pointer:", newValue.CanSet())
+
+	// 重新赋值
+	newValue.SetFloat(77)
+	fmt.Println("new value of pointer:", num)
+
+	////////////////////
+	// 如果reflect.ValueOf的参数不是指针，会如何？
+	pointer = reflect.ValueOf(num)
+	//newValue = pointer.Elem() // 如果非指针，这里直接panic，“panic: reflect: call of reflect.Value.Elem on float
+
+}
+
+//go:embed sloth_getter.tmp
+var sloth_getter string
+
+func TestParseStruct2(t *testing.T) {
+	fmt.Printf("sloth_getter :%s\n", sloth_getter)
+}
