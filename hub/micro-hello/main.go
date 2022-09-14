340e82df9f9282381341f97b196640be3a215e21
ckx0709 <changkx@epshealth.com>
2022-09-14 10:11:43 +0800

    micro

---
+++ hub/micro-hello/main.go
@@ -0,0 +1,38 @@
+/*
+@Time   : 2022/7/14 14:23
+@Author : ckx0709
+@Remark :
+*/
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/micro/micro/v3/service"
+	proto "github.com/micro/services/helloworld/proto"
+)
+
+func main() {
+	// create and initialise a new service
+	srv := service.New()
+
+	// create the proto client for helloworld
+	client := proto.NewHelloworldService("helloworld", srv.Client())
+
+	// call an endpoint on the service
+	rsp, err := client.Call(context.Background(), &proto.CallRequest{
+		Name: "John",
+	})
+	if err != nil {
+		fmt.Println("Error calling helloworld: ", err)
+		return
+	}
+
+	// print the response
+	fmt.Println("Response: ", rsp.Message)
+
+	// let's delay the process for exiting for reasons you'll see below
+	time.Sleep(time.Second * 5)
+}
